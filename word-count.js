e96cbc738a85acc4d3518712cc66147c0c04cc18
garudaramakrishna <ramakrishna_garuda@epam.com>
2017-04-24 13:24:03 +0530

    modified to pass all unit testcases

--- word-count.js
+++ word-count.js
@@ -1,4 +1,14 @@
-
-function countWords(str) 
-{ return str;
-}
+function countWords(str) {
+  var inputString = str.trim(); 
+  var count = 0;
+  if(inputString=='')
+  {
+    return 0;
+  } 
+ // var wordArray = inputString.split(' ');  
+ // for(var i=0; i<wordArray.length; i++){
+   // if(wordArray[i].trim().length > 0){
+   // count = count+1;
+   // }
+ // }//return count;//inputString.split(' ').length;
+  return inputString.split(/\s+/).length; }
