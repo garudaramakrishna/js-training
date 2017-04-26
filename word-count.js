85d906429c6fcf397b205e44cfa5a78fc2087a0c
garudaramakrishna <ramakrishna_garuda@epam.com>
2017-04-26 20:55:14 +0530

    implemented code review comments

--- word-count.js
+++ word-count.js
@@ -1,14 +1,4 @@
 function countWords(str) {
-  var inputString = str.trim(); 
-  var count = 0;
-  if(inputString=='')
-  {
-    return 0;
-  } 
- // var wordArray = inputString.split(' ');  
- // for(var i=0; i<wordArray.length; i++){
-   // if(wordArray[i].trim().length > 0){
-   // count = count+1;
-   // }
- // }//return count;//inputString.split(' ').length;
-  return inputString.split(/\s+/).length; }
+    var inputString = str.trim(); 
+    return inputString == '' ? 0 : inputString.split(/\s+/).length;
+}
